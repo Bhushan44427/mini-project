9bd3387ff5b996ae7c0ca9e6354e7ccc9d1859dd
Bhushan44427 <nagabhushanvelpula1455@gmail.com>
2024-12-20 22:29:24 +0530

    miniproject.java

---
+++ miniproject.java
@@ -0,0 +1,6 @@
+public class Project {
+    public static void main(String[] args) {
+        // Print the desired message to the console
+        System.out.println("THE PROJECT DONE BY THE VEER");
+    }
+}
